e4071cf0acefca5b7aab6bf258e26824b5f789d9
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-01-22 08:06:58 -0500

    header/signin

---
+++ plant-swap/src/components/signin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleInputChange = event => {
+
+    } 
+}
+
+
+export default SignIn;
