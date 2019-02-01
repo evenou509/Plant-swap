1151e33fbb1915f429255732bf9901f258b37a1f
Desiree Solomon <d.d.solomon4@gmail.com>
2019-01-31 21:34:50 -0500

    api

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,23 +1,50 @@
 // create a link to move to the swap page
 
 import React, { Component } from 'react';
-// import axios from "axios";
-// const BASEURL = "https://trefle.io/api/plants?q=";
-// const APIKEY = "&token=S0YveTJTaGgxN3VqbGxOa0ptTWtiQT0";
-// const router = require("express").Router();
-
-// router.get("/search", (req, res) => {
-//   axios
-//     .get(BASEURL + query + APIKEY, { params: req.query })
-//     .then(({ data: { results } }) => res.json(results))
-//     .catch(err => res.status(422).json(err));
-// });
-
+import API from "../utils/API"
 
 class Search extends Component {
+    state = {
+        search_input: "",
+        results: [],
+        error: ""
+      };
+
+    handleInputChange = event => {
+        this.setState({ search_input: event.target.value });
+      };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        API.search()
+          .then(res => {
+            if (res.data.status === "error") {
+              throw new Error(res.data.message);
+            }
+            this.setState({ results: res.data.message, error: "" });
+          })
+          .catch(err => this.setState({ error: err.message }));
+          console.log(this.state.results)
+          console.log(this.state.search_input)
+    
+      };
+
     render() {
         return (
-            <div> Search Page </div>
+            <div>
+                <div> Search Page </div>
+            <form>
+                <label> Search a plant!</label>
+                <input
+            value={this.state.search_input}
+            name="search_Input"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="search here!"
+          />
+           <button onClick={this.handleFormSubmit}>Submit</button>
+            </form>
+            </div>
         )
     }
 }
