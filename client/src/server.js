8b3170d8e5337fbefcfd49f79167534c41a4b87c
Desiree Solomon <d.d.solomon4@gmail.com>
2019-02-05 13:01:06 -0500

    remote

--- client/src/server.js
+++ client/src/server.js
@@ -42,14 +42,14 @@ if (process.env.NODE_ENV === "development") {
 
 
 // Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-//   app.listen(PORT, function() {
-//     console.log(
-//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
 
 module.exports = app;
